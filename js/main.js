2dab8ce9c27cac379bf558d75f38c6ec31ea94f4
IamErfanImmortal <erf.developer88@gmail.com>
2024-02-01 22:30:58 +0100

    add fontawsome and icon and JS files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+$(".inner-switch").on("click", function() {
+    if($("body").hasClass("dark")) {
+        $("body, .content").removeClass("dark");
+        $(".inner-switch").html('<i class="fas fa-moon"></i>'); // استفاده از آیکون ماه
+        $(".inner-switch i").removeClass("on-icon").addClass("off-icon"); // اضافه یا حذف کلاس برای آیکون
+    } else {
+        $("body, .content").addClass("dark");
+        $(".inner-switch").html('<i class="fas fa-sun"></i>'); // استفاده از آیکون خورشید
+        $(".inner-switch i").removeClass("off-icon").addClass("on-icon"); // اضافه یا حذف کلاس برای آیکون
+    }
+});
